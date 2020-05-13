39a2010ef59464f84e4887339d391da5c7948ee0
James Wang <jawang.um@gmail.com>
2020-05-12 19:19:46 -0700

    update

--- js/app.js
+++ js/app.js
@@ -23,8 +23,29 @@ function renderDropDown() {
   }
 }
 
-
 renderDropDown();
+
+
+var itemsArr = ['potato', 'testing', 'potato2'];
+//checkbox renderer
+var check = document.getElementById('checkbox');
+function renderCheck() {
+  for (var i = 0; i < itemsArr.length; i++) {
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.name = itemsArr[i];
+    checkbox.value = itemsArr[i];
+    checkbox.id = itemsArr[i];
+    var label = document.createElement('label');
+    label.htmlFor = "id";
+    label.appendChild(document.createTextNode(itemsArr[i]));
+    check.appendChild(checkbox);
+    check.appendChild(label);
+  }
+}
+renderCheck();
+var button = document.getElementById('catalog');
+button.addEventListener('submit', handleSubmit);
 
 
 // //event listener for click
