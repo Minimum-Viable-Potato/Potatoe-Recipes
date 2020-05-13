197747abe7a9382d5f9c99481a93380c84acd2d3
Ryan Geddes <rgeddes09@gmail.com>
2020-05-12 21:04:29 -0700

    notes from 5-12 lab, DO NOT MERGE

--- js/app.js
+++ js/app.js
@@ -26,26 +26,11 @@ function renderDropDown() {
 
 renderDropDown();
 
+var selectedIngArr = [];
 
-// //event listener for click
-// function handleClick(event) {
-//     // increment our property 'clicks', and generate two new images
-//     var clickedItem = event.target.title;
-//     console.log(clickedItem);
-//     for(var i = 0; i < itemsArr.length; i++){
-//         if(clickedItem === itemsArr[i].name){
-//             itemsArr[i].clicked++;
-//         }
-//     }
-//     clickTracker--;
-//     if(clickTracker <= 0){
-//         stopClicking();
-//         renderChart();
-//         renderList();
-//         saveLocalStorage();
-//         //add render list
-//     }
-//     imageGenerator();
-// }
-// //divEl.addEventListener('click', handleClick);
+function listQ(){
+  selectedIngArr.push(this.value);
+  console.log(selectedIngArr);
+}
+document.getElementById("ingredients_box").onchange = listQ;
 
